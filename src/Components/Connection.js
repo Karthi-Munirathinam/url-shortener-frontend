ebf41f97e973ffe0e3d303b53ae5051301c90941
Karthi <karthiraja268@gmail.com>
2021-09-22 17:53:46 +0530

    heroku url added

--- src/Components/Connection.js
+++ src/Components/Connection.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: "https://mk-password-reset.herokuapp.com"
-    baseURL: "http://localhost:5000"
+    baseURL: "https://mk-url-shortener.herokuapp.com"
+    // baseURL: "http://localhost:5000"
 })
 
 export default instance
