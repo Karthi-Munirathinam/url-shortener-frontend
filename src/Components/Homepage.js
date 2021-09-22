ececd41f69462cd87513475c944328f58b06deca
Karthi <karthiraja268@gmail.com>
2021-09-22 17:57:03 +0530

    loading in homepage added

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 import { useHistory } from 'react-router-dom';
-
+import Loading from './Loading';
 function Homepage() {
+    const [isLoading, setIsLoading] = useState(false);
     const history = useHistory()
     useEffect(() => {
+        setIsLoading(true);
         let token = window.localStorage.getItem("app-token");
         if (token) {
-            history.push('/dashboard')
+            history.push('/dashboard');
+            setIsLoading(false);
         } else {
-            history.push('/login')
+            history.push('/login');
+            setIsLoading(false);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        setIsLoading(false);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <motion.div
@@ -26,9 +31,14 @@ function Homepage() {
             className="container-lg mt-4">
             <div className="row fp-container">
                 <div className="fp-form-container col-12">
-                    <div>
-                        <h5>Url shortener homepage</h5>
-                    </div>
+                    {
+                        isLoading ? <Loading /> : (
+                            <div>
+                                <h5>Url shortener homepage</h5>
+                            </div>
+                        )
+                    }
+
                 </div>
             </div>
         </motion.div >
